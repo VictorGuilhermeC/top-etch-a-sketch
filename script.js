e0a2039257650cfc385b0fb4d8dddebf9388bcc5
VictorGuilhermeC <67566818+VictorGuilhermeC@users.noreply.github.com>
2025-06-26 01:53:26 -0300

    Update script to include 16x16 squares

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
+let n = 16;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < Math.pow(n, 2); i++) {
   const square = document.createElement("div");
   square.className = "square";
   container.appendChild(square);
