5b246884125b6019171bdd47eb0acf31c5da0199
VictorGuilhermeC <67566818+VictorGuilhermeC@users.noreply.github.com>
2025-06-26 01:55:59 -0300

    Update script to add height and width to squares
    Loop adds dynamically width and height to squares according to ratio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container");
 let n = 16;
+const ratio = 100 / n;
 
 for (let i = 0; i < Math.pow(n, 2); i++) {
   const square = document.createElement("div");
