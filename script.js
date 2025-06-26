024d8737049c1ff679e370ce9696129a0c34268a
VictorGuilhermeC <67566818+VictorGuilhermeC@users.noreply.github.com>
2025-06-26 15:15:54 -0300

    Refactor script.js for clarity
    Loop condition was changed from Math.pow(n, 2) to n*n.
    Square's height and width are attributed through string interpolation.

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const container = document.querySelector("#container");
 let n = 16;
 const ratio = 100 / n;
 
-for (let i = 0; i < Math.pow(n, 2); i++) {
+for (let i = 0; i < n * n; i++) {
   const square = document.createElement("div");
   square.className = "square";
+  square.style.height = `${ratio}%`;
+  square.style.width = `${ratio}%`;
   container.appendChild(square);
 }
